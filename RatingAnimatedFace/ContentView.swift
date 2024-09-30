4085942177f8bcb09346f12f234a468353e7ee61
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-09-30 13:26:36 +0600

    Initial Commit

---
+++ RatingAnimatedFace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RatingAnimatedFace
+//
+//  Created by Shishir_Mac on 30/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RatingAnimatedFace/RatingAnimatedFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingAnimatedFaceApp.swift
+//  RatingAnimatedFace
+//
+//  Created by Shishir_Mac on 30/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RatingAnimatedFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
